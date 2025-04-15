ce7b174c77874fb1b7f9ce12e6e9669379473f13
Thuyen Trinh <trinhngocthuyen@gmail.com>
2025-04-15 17:01:38 +0700

    FIX: Encoding

--- examples/LocalPackages/core-utils/Sources/DebugKitObjc/DebugKit.m
+++ examples/LocalPackages/core-utils/Sources/DebugKitObjc/DebugKit.m
@@ -8,7 +8,7 @@
 + (NSString *)loadToken {
   NSBundle *bundle = SWIFTPM_MODULE_BUNDLE;
   NSString *tokenPath = [bundle pathForResource:@"token" ofType:@"txt"];
-  NSString *content = [NSString stringWithContentsOfFile:tokenPath encoding:kCFStringEncodingUTF8 error:nil];
+  NSString *content = [NSString stringWithContentsOfFile:tokenPath encoding:NSUTF8StringEncoding error:nil];
   return [content stringByReplacingOccurrencesOfString:@"\n" withString:@""];
 }
 @end
