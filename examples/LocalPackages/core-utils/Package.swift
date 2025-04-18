7b85c1c684cee8a2c15b91d4b6f9669b89d54f40
Thuyen Trinh <trinhngocthuyen@gmail.com>
2025-04-18 10:49:56 +0700

    CHORE: Lower min iOS version

--- examples/LocalPackages/core-utils/Package.swift
+++ examples/LocalPackages/core-utils/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "CoreUtils",
-  platforms: [.iOS(.v18)],
+  platforms: [.iOS(.v17)],
   products: [
     .library(name: "Swizzler", targets: ["Swizzler"]),
     .library(name: "ResourceKit", targets: ["ResourceKit"]),
