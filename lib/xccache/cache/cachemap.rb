a8b624693c824f412ed7f29f72cc774f5f57980b
Thuyen Trinh (Chris) <trinhngocthuyen@gmail.com>
2025-04-21 11:33:45 +0700

    FEAT: Cachemap viz (#30)

--- lib/xccache/cache/cachemap.rb
+++ lib/xccache/cache/cachemap.rb
@@ -7,6 +7,10 @@ module XCCache
         raw["deps"] ||= {}
       end
 
+      def depgraph_data
+        raw["depgraph"] ||= {}
+      end
+
       def cache_data
         raw["cache"] ||= { "hit" => [], "missed" => {} }
       end
@@ -16,15 +20,43 @@ module XCCache
       end
 
       def sync!(lockfile, depmap, pkg_descs_by_name)
-        targets = lockfile.product_dependencies.flat_map { |p| depmap[p] || [p] }.uniq
-        pkg_descs = pkg_descs_by_name.values.flatten.uniq
-        binary_target_names = pkg_descs.flat_map(&:binary_targets).map(&:full_name)
+        @lockfile, @depmap, @pkg_descs_by_name = lockfile, depmap, pkg_descs_by_name
+        gen_cache_data
+        gen_depgraph_data
+        print_stats
+        save
+      end
 
+      def result_type(name)
+        return :ignored if ignore?(name)
+        return :hit if hit?(name)
+        :missed
+      end
+
+      def hit?(name)
+        cache_data["hit"].include?(name) && !ignore?(name)
+      end
+
+      def hit_product?(name)
+        cache_data["hit_products"].include?(name) && !ignore?(name)
+      end
+
+      def missed_targets
+        cache_data
+          .fetch("missed_products", {}).keys
+          .flat_map { |p| deps_data[p] }.uniq
+          .reject { |x| hit?(x) || ignore?(x) }
+          .map { |x| x.split("/").last }
+      end
+
+      private
+
+      def gen_cache_data
+        binary_target_names = @pkg_descs_by_name.values.flatten.uniq.flat_map(&:binary_targets).map(&:full_name)
+        targets = @lockfile.product_dependencies.flat_map { |p| @depmap[p] || [p] }.uniq
         targets.difference(binary_target_names).each do |d|
-          slug, name = d.split("/")
-          pkg_desc = pkg_descs_by_name[slug]
-          bpath = binary_path(name)
-          bpath_with_checksum = binary_path(name, checksum: pkg_desc.checksum)
+          bpath = binary_path(File.basename(d))
+          bpath_with_checksum = binary_path(File.basename(d), checksum: pkg_desc_of(d).checksum)
           # If checksum matches, create symlink from `A-abc123.xcframework` -> `A.framework`
           # Otherwise, remove symlink `A.xcframework`
           if bpath_with_checksum.exist?
@@ -43,44 +75,60 @@ module XCCache
         end
 
         hit_products, missed_products = [], {}
-        lockfile.product_dependencies.each do |product|
-          deps = depmap[product] || [product]
+        @lockfile.product_dependencies.each do |product|
+          deps = @depmap[product] || [product]
           missing = deps.intersection(missed.keys)
           hit_products << product if missing.empty?
           missed_products[product] = missing.map { |d| "#{d} (#{missed[d]})" }.join(", ") unless missing.empty?
         end
 
-        lockfile.product_dependencies_by_targets.each do |target_name, products|
+        @lockfile.product_dependencies_by_targets.each do |target_name, products|
           targets_data["#{target_name}.xccache"] = products.flat_map do |product|
             next product unless hit_products.include?(product)
-            depmap[product].map { |x| "#{x}.binary" }
+            @depmap[product].map { |x| "#{x}.binary" }
           end.uniq
         end
 
-        raw["deps"] = depmap
+        raw["deps"] = @depmap
         raw["cache"] = {
           "hit" => hit,
           "missed" => missed,
           "hit_products" => hit_products,
           "missed_products" => missed_products,
         }
-        print_stats
-        save
       end
 
-      def hit?(name)
-        cache_data["hit"].include?(name) && !ignore?(name)
-      end
+      def gen_depgraph_data
+        nodes, edges, parents = [], [], {}
+        to_visit = @lockfile.product_dependencies_by_targets.flat_map do |target_name, deps|
+          nodes << { :id => target_name, :label => target_name, :agg => true }
+          targets = deps.flat_map { |d| pkg_desc_of(d).targets_of_product(File.basename(d)) }
+          targets.each { |t| edges << { :source => target_name, :target => t.full_name } }
+          targets
+        end.uniq
 
-      def missed_targets
-        cache_data
-          .fetch("missed_products", {}).keys
-          .flat_map { |p| deps_data[p] }.uniq
-          .reject { |x| hit?(x) || ignore?(x) }
-          .map { |x| x.split("/").last }
-      end
+        until to_visit.empty?
+          target = to_visit.pop
+          nodes << { :id => target.full_name, :label => target.name }
+          target.direct_dependency_targets.each do |t|
+            parents[t.full_name] ||= []
+            parents[t.full_name] << target.full_name
+            edges << { :source => target.full_name, :target => t.full_name }
+            to_visit << t unless to_visit.include?(t)
+          end
+        end
+        nodes.uniq!
 
-      private
+        nodes_by_name = nodes.to_h { |h| [h[:id], h] }
+        nodes.reject { |h| h[:agg] }.each do |h|
+          h[:cache] = result_type(h[:id]) unless h.key?(:cache)
+          next if h[:cache] == :hit || !parents.key?(h[:id])
+          parents[h[:id]].each do |p|
+            nodes_by_name[p][:cache] = :missed
+          end
+        end
+        raw["depgraph"] = { :nodes => nodes, :edges => edges }
+      end
 
       def binary_path(name, checksum: nil)
         suffix = checksum.nil? ? "" : "-#{checksum}"
@@ -105,6 +153,10 @@ module XCCache
             • Missed (#{missed_products.count}):\n#{JSON.pretty_generate(missed_products).yellow.dark}
           -------------------------------------------------------------------
         DESC
+      end
+
+      def pkg_desc_of(d)
+        @pkg_descs_by_name[d.split("/")[0]] unless @pkg_descs_by_name.nil?
       end
     end
   end
